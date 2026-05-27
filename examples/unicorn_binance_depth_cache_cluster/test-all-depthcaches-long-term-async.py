ba3ae725ce03b50ce9aaf75e1d94d672cc0326b8
oliver-zehentleitner <privat@me.com>
2026-05-27 12:04:39 +0200

    k8s

--- examples/unicorn_binance_depth_cache_cluster/test-all-depthcaches-long-term-async.py
+++ examples/unicorn_binance_depth_cache_cluster/test-all-depthcaches-long-term-async.py
@@ -44,6 +44,7 @@ async def main():
         print(f"Successful working caches: {len(working_caches)}")
         for error in errors:
             print(f"ERROR: {error}: {errors[error]}")
+        exit(0)
         await asyncio.sleep(1)
 
 try:
